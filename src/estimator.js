a5476721442a5447ffc07d71476fd5387d78d07f
tos4christ <gnztrades@gmail.com>
2020-04-09 21:15:12 +0100

    run eslint on challenge to meet standard

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,24 @@
 const covid19ImpactEstimator = (data) => {
-    const input = {...data};
-    const impact = {};
-    const severeImpact = {};
-    let duration, requestedTime = 0;
-    if (input.periodType === "days") {
-        duration = input.timeToElapse;
-    } else if (input.periodType === "weeks") {
-        duration = input.timeToElapse * 7;
-    } else if (input.periodType === "months") {
-        duration = input.timeToElapse * 30;
-    }
-    impact.currentlyInfected = input.reportedCases * 10;
-    severeImpact.currentlyInfected = input.reportedCases * 50;
+  const input = { ...data };
+  const impact = {};
+  const severeImpact = {};
+  let duration;
+  let requestedTime = 0;
+  if (input.periodType === 'days') {
+    duration = input.timeToElapse;
+  } else if (input.periodType === 'weeks') {
+    duration = input.timeToElapse * 7;
+  } else if (input.periodType === 'months') {
+    duration = input.timeToElapse * 30;
+  }
+  impact.currentlyInfected = input.reportedCases * 10;
+  severeImpact.currentlyInfected = input.reportedCases * 50;
 
-    if (duration >= 3) {
-        requestedTime = duration / 3
-    }
-    impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, requestedTime);
-    severImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * Math.pow(2, requestedTime);
+  if (duration >= 3) {
+    requestedTime = duration / 3;
+  }
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** requestedTime);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** requestedTime);
 };
 
 export default covid19ImpactEstimator;
