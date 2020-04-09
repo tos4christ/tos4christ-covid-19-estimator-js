64fc646abfe7628819df35e73d3a7f8be135429b
tos4christ <gnztrades@gmail.com>
2020-04-09 21:44:35 +0100

    challenge 2 completed with code linted properly

--- src/estimator.js
+++ src/estimator.js
@@ -11,14 +11,23 @@ const covid19ImpactEstimator = (data) => {
   } else if (input.periodType === 'months') {
     duration = input.timeToElapse * 30;
   }
-  impact.currentlyInfected = input.reportedCases * 10;
-  severeImpact.currentlyInfected = input.reportedCases * 50;
-
   if (duration >= 3) {
     requestedTime = duration / 3;
   }
+  // Currently Infected
+  impact.currentlyInfected = input.reportedCases * 10;
+  severeImpact.currentlyInfected = input.reportedCases * 50;
+  // Infections By Requested Time
   impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** requestedTime);
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** requestedTime);
+  // Severe Cases By Requested Time
+  impact.severeCasesByRequestedTime = (0.15 * impact.infectionsByRequestedTime);
+  severeImpact.severeCasesByRequestedTime = (0.15 * severeImpact.infectionsByRequestedTime);
+  // Hospital Beds by Requested Time
+  impact.hospitalBedsByRequestedTime = (0.35 * input.totalHospitalBeds)
+   - impact.severeCasesByRequestedTime;
+  severeImpact.hospitalBedsByRequestedTime = (0.35 * input.totalHospitalBeds)
+   - severeImpact.severeCasesByRequestedTime;
 };
 
 export default covid19ImpactEstimator;
