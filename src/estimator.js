897ecbbc360f8d6a78c59d73fc7507be409dd6be
tos4christ <gnztrades@gmail.com>
2020-04-09 11:29:26 +0100

    complete challenge 1 currently infected and infections by requested time

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = {...data};
+    const impact = {};
+    const severeImpact = {};
+    let duration, requestedTime = 0;
+    if (input.periodType === "days") {
+        duration = input.timeToElapse;
+    } else if (input.periodType === "weeks") {
+        duration = input.timeToElapse * 7;
+    } else if (input.periodType === "months") {
+        duration = input.timeToElapse * 30;
+    }
+    impact.currentlyInfected = input.reportedCases * 10;
+    severeImpact.currentlyInfected = input.reportedCases * 50;
+
+    if (duration >= 3) {
+        requestedTime = duration / 3
+    }
+    impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2, requestedTime);
+    severImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * Math.pow(2, requestedTime);
+};
 
 export default covid19ImpactEstimator;
