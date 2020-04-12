c0a91c956777c4be183ea924f8f845f37e88915c
tos4christ <gnztrades@gmail.com>
2020-04-12 19:09:58 +0100

    set the connection string of the databse to heroku uri

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,7 @@ const app = express();
 const stats = new StatsD();
 
 const pool = new Pool({
-  host: process.env.PGHOST,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  port: process.env.PGPORT
+  connectionString: process.env.DATABASE_URL
 });
 
 pool.on('connect', () => console.log('Connected to the database'));
