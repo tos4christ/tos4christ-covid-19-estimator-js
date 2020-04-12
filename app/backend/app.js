b14383da5e24d088f21705d26c8f26697dd175e6
tos4christ <gnztrades@gmail.com>
2020-04-13 00:17:47 +0100

    updated server to meet revised guidelines

--- app/backend/app.js
+++ app/backend/app.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(responseTime((req, res, time) => {
-  const log = `${Date.now()}    ${req.originalUrl}    done in ${time.toFixed(2)} seconds`;
+  const log = `${req.method}    ${req.originalUrl}    ${req.statusCode}   ${parseInt(time, 10)}ms`;
   pool.query(query.post, [log]);
 }));
 
