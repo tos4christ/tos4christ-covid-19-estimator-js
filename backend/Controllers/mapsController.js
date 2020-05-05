37fcceecf4e8bd6a1dfde2eb808dd45b6b986ea0
tos4christ <gnztrades@gmail.com>
2020-05-05 23:36:09 +0100

    Properly exported mapsApi module

--- backend/Controllers/mapsController.js
+++ backend/Controllers/mapsController.js
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
 /* locs would be replaced by the coordinates sent to the server from the client */
-const locs = { lat: 6.3445645, lng: 3.4533255 };
+// const locs = { lat: 6.3445645, lng: 3.4533255 };
 /* firestations would be stored and pulled from the database, this is just a mock */
 const fireStations = [
   { Lekki: { lat: 6.4355376, lng: 3.4829765 } },
@@ -29,11 +29,11 @@ function findClosestStation(accidentLocation) {
 }
 
 /* Function to find the optimal route between the closest firestation and the accident location */
-function optimalRoute() {
+// function optimalRoute() {
 
-}
+// }
 
-const mapsApi = (req, res, next) => {
+const mapsApi = (req, res) => {
   // Coordinates of the accident location
   const { accidentLocation } = req.body;
   const closestStation = findClosestStation(accidentLocation);
@@ -44,8 +44,8 @@ const mapsApi = (req, res, next) => {
 };
 
 /* This is to manually test the function and see that it works */
-const cs = findClosestStation(locs);
-console.log(cs);
+// const cs = findClosestStation(locs);
+// console.log(cs);
 
 /* This controller would be exported to the routes module to be used in the maps logic */
-// export default mapsApi;
+export default mapsApi;
