0309b9cdca5641fa92887d24f82a7b3478891c2d
tos4christ <gnztrades@gmail.com>
2020-06-05 22:40:31 +0100

    add responder signin route and controller

--- backend/Controllers/responderController.js
+++ backend/Controllers/responderController.js
@@ -38,6 +38,42 @@ respondersController.register = (req, res) => {
     });
 };
 
+respondersController.signin = (req, res) => {
+  const { email, password } = req.body;
+  if (!email && !password) {
+    res.status(400).json({
+      status: 'error',
+      error: 'Email and password field cannot be empty'
+    });
+    return;
+  }
+  RespondersModel.getRespondersByEmail(email).then((result) => {
+    const passwordMatch = encoder.decode(password, result.password);
+    if (passwordMatch) {
+      const { _id: userId } = result;
+      // inside the database operation, store the jwt
+      const token = jwt.sign({
+        sub: userId
+      }, process.env.TOKENKEY, { expiresIn: 1440 });
+      // the body to send to front end
+      const responseBody = {
+        status: 'Success',
+        data: {
+          message: 'Your are now signed in',
+          token,
+          userId
+        }
+      };
+      res.status(200).json(responseBody);
+    } else {
+      res.status(401).json({
+        status: 'error',
+        error: 'Password does not match'
+      });
+    }
+  });
+};
+
 respondersController.fetchAll = (req, res) => {
   RespondersModel.getAll()
     .then((result) => {
--- backend/Routers/signupResponderRoute.js
+++ backend/Routers/signupResponderRoute.js
@@ -4,5 +4,6 @@ import responderController from '../Controllers/responderController';
 const router = new Router();
 
 router.post('/', responderController.register);
+router.post('/signin', responderController.signin);
 
 export default router;
