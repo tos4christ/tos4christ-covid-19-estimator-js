06ad1d85cd8bcd6ec2face7f8d942b7c11dd8965
tos4christ <gnztrades@gmail.com>
2020-06-05 19:29:51 +0100

    add access control headers to app

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,19 @@ config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
